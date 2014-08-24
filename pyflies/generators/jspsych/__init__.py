fd9ba4edd07161f3a35979ec10fceec5d0eaa93f
Igor Dejanovic <igor.dejanovic@gmail.com>
2014-08-25 01:25:13 +0200

    Generator for pyPsych

---
+++ pyflies/generators/jspsych/__init__.py
@@ -0,0 +1,30 @@
+"""
+Generator for `jsPsych`_ JavaScript library for behavioral experiments.
+""" 
+
+name = "jsPsych"
+description = "jsPsych - A JavaScript library of online behavioral experiments"
+
+from jinja2 import Template
+
+def generate(model, target_folder):
+    """
+    Generates a jsPsych experiment from the experiment model.
+
+    Args:
+        model(pyFlies model): A graph of Python objects defining experiment.
+        target_folder(str): A name of the folder where generated code should be placed.
+    """
+
+
+    # Generate index template.
+    with open('./templates/index.html.template', 'r') as f:
+        index_template = f.read()
+
+    template = Template(index_template)
+
+    with open(os.path.join(target_folder, 'index.html'), 'w') as f:
+        f.write(template.render(model=model))
+
+
+

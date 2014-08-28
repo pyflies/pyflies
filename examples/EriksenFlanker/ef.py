f47d79da40cd2719179fb6bb7dadae3e841b7f83
Igor Dejanovic <igor.dejanovic@gmail.com>
2014-08-28 23:41:39 +0200

    EriksenFlanker example test py file

---
+++ examples/EriksenFlanker/ef.py
@@ -0,0 +1,12 @@
+from textx.metamodel import metamodel_from_file
+from textx.export import metamodel_export, model_export
+
+pyflies_mm = metamodel_from_file(
+    '../../pyflies/lang/pyflies.tx',
+    builtins={'categorisation': 'categorisation',
+              'visual': 'visual'})
+metamodel_export(pyflies_mm, 'pyflies_meta.dot')
+
+experiment = pyflies_mm.model_from_file('EriksenFlanker.pf')
+model_export(experiment, 'EriksenFlanker.dot')
+

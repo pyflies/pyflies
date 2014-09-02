350938ef166393cef74fd620dfba58c3c5f85f51
Igor Dejanovic <igor.dejanovic@gmail.com>
2014-09-02 17:01:50 +0200

    Custom experiment visualization finished

--- pyflies/export.py
+++ pyflies/export.py
@@ -14,60 +14,105 @@ digraph pyflies {
 
 '''
 
+# Cluster for sequence and randomize blocks
+cluster = 1
+last_node = 'start'
+node_num = 1
+
 
 def custom_export(model, file_name):
 
-    with open(file_name, 'w') as f:
-        f.write(HEADER)
+    global cluster, last_node, node_num
 
-        # Find experiment
-        experiment = next(ifilter(
-            lambda x: x.__class__.__name__ == "Experiment",
-            model.elements), None)
+    # Cluster for sequence and randomize blocks
+    cluster = 1
+    node_num = 1
+    last_node = 'start'
 
-        last_node = 'start'
+    # Find experiment
+    experiment = next(ifilter(
+        lambda x: x.__class__.__name__ == "Experiment",
+        model.elements), None)
 
-        if experiment:
-            for e in experiment.elements:
-                clsname = e.__class__.__name__
-                if clsname == "TextReference":
-                    node_name = e.text.name
-                    f.write('{} [shape=note, fillcolor=green, label="{}"];'
-                            .format(e.text.name, e.text.content))
-                elif clsname == "SubjectReference":
-                    node_name = e.subject.name
-                    attr_str = ""
-                    for attr in e.subject.attribute:
-                        if attr.type.__class__.__name__ == "Enum":
-                            attr_type = "[{}]".format(
-                                ", ".join(attr.type.values))
-                        else:
-                            attr_type = attr.type
-                        attr_str += "{}:{}\\l".format(attr.name, attr_type)
+    if experiment:
 
-                    f.write('{} [shape=record, label="{{Subject|{}}}"];'
-                            .format(node_name, attr_str))
-                elif clsname == "TestReference":
-                    node_name = e.test.name
-                    color = "green" if e.test.practice else "red"
-                    randomize = "randomize" if e.test.randomize else ""
-                    f.write(''' {} [shape=doubleoctagon,fillcolor={}, label=<
+        def _render_dot(e):
+            """
+            Renders experiment element.
+            """
+
+            global cluster, last_node, node_num
+
+            clsname = e.__class__.__name__
+
+            if clsname == "TextReference":
+                f.write('{} [shape=note, fillcolor=lawngreen, label="{}"];\n'
+                        .format(node_num, e.text.content))
+                node_num += 1
+            elif clsname == "SubjectReference":
+                attr_str = ""
+                for attr in e.subject.attribute:
+                    if attr.type.__class__.__name__ == "Enum":
+                        attr_type = "[{}]".format(
+                            ", ".join(attr.type.values))
+                    else:
+                        attr_type = attr.type
+                    attr_str += "{}:{}\\l".format(attr.name, attr_type)
+
+                f.write('{} [shape=record, label="{{Subject|{}}}"];\n'
+                        .format(node_num, attr_str))
+                node_num += 1
+            elif clsname == "TestReference":
+                color = "lawngreen" if e.test.practice else "red"
+                randomize = '<FONT POINT-SIZE="15">randomize</FONT><BR/>'\
+                    if e.test.randomize else ""
+
+                f.write(''' {} [shape=doubleoctagon,fillcolor={}, label=<
 <FONT POINT-SIZE="20">{}</FONT><BR/>
 <FONT POINT-SIZE="15">tmin:{}</FONT><BR/>
 <FONT POINT-SIZE="15">tmax:{}</FONT><BR/>
 <FONT POINT-SIZE="15">twait:{}</FONT><BR/>
-                            {}
-                            >]'''.format(
-                            node_name, color, e.test.name, e.test.tmin,
-                            e.test.tmax, e.test.twait, randomize))
-                elif clsname == "Sequence":
-                    pass
-                elif clsname == "Randomize":
-                    pass
+{}
+                        >];\n'''.format(
+                        node_num, color, e.test.name, e.test.tmin,
+                        e.test.tmax, e.test.twait, randomize))
+                f.write('{} -> {} [dir=back, label="{}"];\n'.format(
+                    node_num, node_num, e.test.trials))
+                node_num += 1
+            elif clsname == "Sequence":
+                f.write('''subgraph cluster{} {{
+                            label="";
+                            style=dotted;
+                            penwidth=2;
+                            color=black;\n\n'''.format(cluster))
+                cluster += 1
+                for x in e.elements:
+                    _render_dot(x)
+                f.write('}\n')
+            elif clsname == "Randomize":
+                f.write('''subgraph cluster{} {{
+                            style=solid;
+                            color=yellow;
+                            penwidth=2;
+                            label = "randomize";\n\n'''.format(cluster))
+                cluster += 1
+                for x in e.elements:
+                    _render_dot(x)
+                f.write('}\n')
 
-                f.write('{} -> {};'.format(last_node, node_name))
-                last_node = node_name
 
-        f.write("{} -> end;".format(last_node))
-        f.write('\n}\n')
+        with open(file_name, 'w') as f:
+            f.write(HEADER)
+
+            for elem in experiment.elements:
+                _render_dot(elem)
+
+            # Connections
+            if node_num > 1:
+                f.write('start -> 1;\n')
+            for n in range(1, node_num-1):
+                f.write('{} -> {};\n'.format(n, n+1))
+
+            f.write("{} -> end;\n".format(n+1))
+            f.write('\n}\n')
 

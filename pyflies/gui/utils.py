68973350961eae89d093c9e60f796300bd3c8951
Igor Dejanovic <igor.dejanovic@gmail.com>
2014-09-09 19:39:46 +0200

    Util function for information dialog box.

--- pyflies/gui/utils.py
+++ pyflies/gui/utils.py
@@ -3,7 +3,15 @@ from gi.repository import Gtk
 
 def show_error(message):
     dialog = Gtk.MessageDialog(None, 0, Gtk.MessageType.ERROR,
-                               Gtk.ButtonsType.CANCEL, "Error")
+                               Gtk.ButtonsType.OK, "Error")
+    dialog.format_secondary_text(message)
+    dialog.run()
+    dialog.destroy()
+
+
+def show_info(title, message):
+    dialog = Gtk.MessageDialog(None, 0, Gtk.MessageType.INFO,
+                               Gtk.ButtonsType.OK, title)
     dialog.format_secondary_text(message)
     dialog.run()
     dialog.destroy()

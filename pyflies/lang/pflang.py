effc43bea55597648a4e5806756f898eb63ffa27
Igor Dejanovic <igor.dejanovic@gmail.com>
2015-11-25 23:08:12 +0100

    pflang module can be run for debuging.

--- pyflies/lang/pflang.py
+++ pyflies/lang/pflang.py
@@ -1,11 +1,13 @@
 import os
 from textx.metamodel import metamodel_from_file
 from textx.export import metamodel_export
-from pyflies.lang.model_processor import pyflies_model_processor
-
-pyflies_mm = metamodel_from_file(
-    os.path.join(os.path.dirname(__file__), 'pyflies.tx'))
-pyflies_mm.register_model_processor(pyflies_model_processor)
 
 if __name__ == '__main__':
-    metamodel_export(pyflies_mm, 'pyflies_meta.dot')
+    pyflies_mm = metamodel_from_file(
+        os.path.join(os.path.dirname(__file__), 'pyflies.tx'), debug=True)
+else:
+    from pyflies.lang.model_processor import pyflies_model_processor
+    pyflies_mm = metamodel_from_file(
+        os.path.join(os.path.dirname(__file__), 'pyflies.tx'))
+    pyflies_mm.register_model_processor(pyflies_model_processor)
+

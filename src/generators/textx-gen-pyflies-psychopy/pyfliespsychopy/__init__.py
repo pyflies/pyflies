df870b77d6e828524c89c58a84170298e4fc8adc
Igor Dejanovic <igor.dejanovic@gmail.com>
2020-08-05 17:37:16 +0200

    Move PsychoPy generator code to the new project

--- src/generators/textx-gen-pyflies-psychopy/pyfliespsychopy/__init__.py
+++ src/generators/textx-gen-pyflies-psychopy/pyfliespsychopy/__init__.py
@@ -1,24 +1,59 @@
-import os
-from functools import partial
-from textx import generator, gen_file, get_output_filename
+import re
+from os.path import basename, splitext, join, dirname
+from textx import generator
+from textxjinja import textx_jinja_generator
 
 __version__ = "0.1.0.dev"
 
 
 @generator('pyflies', 'psychopy')
-def pyflies_generate_psychopy(metamodel, model, output_path, overwrite, debug, **custom_args):
-    "Generator for generating psychopy from pyflies descriptions"
+def pyflies_generate_psychopy(metamodel, model, output_path, overwrite, debug,
+                              **custom_args):
+    "Generator for generating PsychoPy code from pyFlies descriptions"
 
-    output_file = get_output_filename(model.file_name, output_path, '*.test')
-    gen_file(model.file_name, output_file,
-             partial(generator_callback, model, output_file),
-             overwrite,
-             success_message='To convert to png run "dot -Tpng -O {}"'
-             .format(os.path.basename(output_file)))
+    # template directory
+    template_folder = join(dirname(__file__), 'templates')
+
+    if output_path is None:
+        output_path = dirname(model._tx_filename)
+
+    file_name = basename(splitext(model._tx_filename)[0])
+    config = {'m': model,
+              'file_name': file_name,
+              'responseMap': {}}
+
+    for target in model.targets:
+        if target.name.lower() == 'psychopy':
+            if target.output:
+                output_path = target.output
+
+            for rm in target.responseMap:
+                config['responseMap'][rm.name] = rm.target
+
+            for tp in target.targetParam:
+                config[tp.name] = tp.value
+
+    filters = {
+        'striptabs': striptabs,
+        'duration': duration
+    }
+
+    # call the generator
+    textx_jinja_generator(template_folder, output_path, config, overwrite, filters)
 
 
-def generator_callback(model, output_file):
-    """
-    A generator function that produce output_file from model.
-    """
-    # TODO: Write here code that produce generated output
+# Jinja filters
+
+def striptabs(s):
+    return re.sub(r'^[ \t]+', '', s, flags=re.M)
+
+
+def duration(s):
+    if type(s.duration) is int:
+        print(s.shape)
+    if type(s.duration) is list:
+        return str(s.duration[0].value)
+    if s.duration and s.duration.value:
+        return str(s.duration.value)
+    else:
+        return "(%d, %d)" % (s.duration._from, s.duration.to)

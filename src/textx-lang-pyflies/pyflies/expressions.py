290f8bebd274933ab9053c3d1ce9eb3d12c87912
Igor Dejanovic <igor.dejanovic@gmail.com>
2020-09-18 21:36:53 +0200

    Add support for compound types, messages and string interpolation

--- src/textx-lang-pyflies/pyflies/expressions.py
+++ src/textx-lang-pyflies/pyflies/expressions.py
@@ -1,5 +1,6 @@
 import sys
 import inspect
+import random
 from operator import or_, and_, not_, eq, ne, lt, gt, le, ge, add, sub, mul, truediv, neg
 from functools import reduce
 from itertools import cycle
@@ -22,7 +23,7 @@ class VariableAssignment(CustomClass):
         model = get_model(self)
         if not hasattr(model, 'var_vals'):
             model.var_vals = {}
-        model.var_vals[name] = value
+        model.var_vals[name] = value.eval() if isinstance(value, ExpressionElement) else value
 
     def __repr__(self):
         return '{} = {}'.format(self.name, self.value)
@@ -42,6 +43,45 @@ class ExpressionElement(CustomClass):
         else:
             # if a single operation, cycle
             return cycle([self.operation])
+
+
+class String(ExpressionElement):
+    def eval(self):
+        model = get_model(self)
+        try:
+            return self.val.format(**model.var_vals)
+        except KeyError as k:
+            raise PyFliesException('Undefined variable "{}"'.format(k.args[0]))
+        except AttributeError:
+            if '{' in self.val.replace('{{', ''):
+                raise PyFliesException('Undefined variables in "{}"'.format(self.val))
+            return self.val
+
+
+class List(ExpressionElement):
+    def eval(self):
+        res = []
+        for v in self.values:
+            try:
+                res.append(v.eval())
+            except AttributeError:
+                res.append(v)
+        return res
+
+
+class Range(ExpressionElement):
+    def eval(self):
+        return list(range(self.lower, self.upper + 1))
+
+
+class MessageExpression(ExpressionElement):
+    def eval(self):
+        value = self.value.eval()
+        if self.message == 'shuffle':
+            random.shuffle(value)
+            return value
+        else:
+            return random.choice(value)
 
 
 class BinaryOperation(ExpressionElement):
--- src/textx-lang-pyflies/tests/test_language.py
+++ src/textx-lang-pyflies/tests/test_language.py
@@ -37,18 +37,23 @@ def test_time_references():
     assert ref.time == 10
 
 
-def test_expressions():
+def test_expressions_arithmetic():
     """
-    Test Pyflies expressions.
+    Test Pyflies arithmetic expressions.
     """
 
     mm = metamodel_from_file(join(this_folder, 'expression.tx'), classes=custom_exp_classes)
 
-    # Basic arithmetic
     exp = mm.model_from_str('2 + 3 * 2 * 3 / 5 - 1')
     assert exp.exp.eval() == 4.6
 
-    # comparisons and booleans
+def test_expressions_comparison_boolean():
+    """
+    Test Pyflies boolean expressions.
+    """
+
+    mm = metamodel_from_file(join(this_folder, 'expression.tx'), classes=custom_exp_classes)
+
     exp = mm.model_from_str('2 > 1 and 6 <= 8')
     assert exp.exp.eval() is True
     exp = mm.model_from_str('2 > 1 and 6 >= 8')
@@ -61,6 +66,68 @@ def test_expressions():
     # This will be True only if `and` is evaluated before `or`
     exp = mm.model_from_str('2 > 1 or 6 >= 8 and 9 < 9')
     assert exp.exp.eval() is True
+
+
+def test_compound_types():
+    mm = metamodel_from_file(join(this_folder, 'expression.tx'), classes=custom_exp_classes)
+
+    m = mm.model_from_str('[1, 2, ["some string", 4.5], 3]')
+    meval = m.exp.eval()
+    assert meval == [1, 2, ['some string', 4.5], 3]
+
+    m = mm.model_from_str('10..20')
+    meval = m.exp.eval()
+    assert meval == list(range(10, 21))
+
+
+def test_expresions_messages():
+    """
+    Test sending messages to compound types
+    """
+    mm = metamodel_from_file(join(this_folder, 'expression.tx'), classes=custom_exp_classes)
+
+    m = mm.model_from_str('10..20 shuffle')
+    meval = m.exp.eval()
+    assert all([x in meval for x in range(10, 21)])
+
+    m = mm.model_from_str('[1, 2, "some value"] shuffle')
+    meval = m.exp.eval()
+    assert all([x in meval for x in [1, 2, "some value"]])
+
+    m = mm.model_from_str('10..20 choose')
+    meval = m.exp.eval()
+    assert meval in range(10, 21)
+
+    m = mm.model_from_str('[1, 2, "some value"] choose')
+    meval = m.exp.eval()
+    assert meval in [1, 2, 'some value']
+
+
+def test_string_interpolation():
+    mm = metamodel_from_file(join(this_folder, 'variables.tx'), classes=custom_exp_classes)
+
+    m = mm.model_from_str('''
+    a = 5
+    b = "some string"
+    Pi = 3.14
+
+    "this is {a} interpolation {b} {Pi}"
+    ''')
+    assert m.exp.eval() == 'this is 5 interpolation some string 3.14'
+
+    with pytest.raises(PyFliesException, match=r'.*Undefined variable.*'):
+        m = mm.model_from_str('''
+        a = 5
+
+        "this is {a} interpolation {b}"
+        ''')
+        m.exp.eval()
+
+    with pytest.raises(PyFliesException, match=r'.*Undefined variable.*'):
+        m = mm.model_from_str('''
+        "this is {a} interpolation {b}"
+        ''')
+        m.exp.eval()
 
 
 def test_variables():
@@ -98,3 +165,23 @@ def test_variables():
         ''')
         m.exp.eval()
 
+
+def test_simuli_definition():
+
+    mm = metamodel_from_file(join(this_folder, 'stimuli.tx'), classes=custom_exp_classes)
+
+    m = mm.model_from_str('at 100 circle(position 0, radius 20) for 200')
+    stim = m.stimuli[0]
+    assert stim.duration == 200
+    assert stim.at.time == 100
+    assert stim.stimulus.name == 'circle'
+    assert stim.stimulus.params[0].name == 'position'
+    assert stim.stimulus.params[0].value.eval() == 0
+    assert stim.stimulus.params[1].name == 'radius'
+    assert stim.stimulus.params[1].value.eval() == 20
+
+    m = mm.model_from_str('at 100 record')
+    stim = m.stimuli[0]
+    assert stim.duration == 0
+    assert stim.record
+    assert stim.at.time == 100

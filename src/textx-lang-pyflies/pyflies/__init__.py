3b729d428647d4e8c2f276282b24b40184e15e0b
Igor Dejanovic <igor.dejanovic@gmail.com>
2020-09-26 18:17:54 +0200

    Fix language registration

--- src/textx-lang-pyflies/pyflies/__init__.py
+++ src/textx-lang-pyflies/pyflies/__init__.py
@@ -1,15 +1,16 @@
 import os
 from textx import language, metamodel_from_file
 from .model_processor import processor
+from .custom_classes import custom_classes
 
 __version__ = "0.4.0.dev"
 
 
 @language('pyflies', '*.pf')
 def pyflies_language():
-    "pyflies language"
+    "A language for psychology experiments specification"
     current_dir = os.path.dirname(__file__)
-    mm = metamodel_from_file(os.path.join(current_dir, 'pyflies.tx'))
+    mm = metamodel_from_file(os.path.join(current_dir, 'pyflies.tx'), classes=custom_classes)
 
     # Here if necessary register object processors or scope providers
     # http://textx.github.io/textX/stable/metamodel/#object-processors

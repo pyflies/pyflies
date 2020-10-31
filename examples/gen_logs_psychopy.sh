18b50d1bab9b5d0cf1a3162e1471ae21802bf08c
Igor Dejanovic <igor.dejanovic@gmail.com>
2020-10-31 11:46:34 +0100

    Regenerate examples

--- examples/gen_logs_psychopy.sh
+++ examples/gen_logs_psychopy.sh
@@ -1,7 +1,10 @@
 #!/bin/sh
 
-for model in EriksenFlanker/EriksenFlanker.pf Simon/Simon.pf PosnerCueing/PosnerCueing.pf Parity/Parity.pf blocking/blocking.pf
+for model in HelloWorld/HelloWorld.pf EriksenFlanker/EriksenFlanker.pf Simon/Simon.pf PosnerCueing/PosnerCueing.pf Parity/Parity.pf blocking/blocking.pf
 do
     textx generate $model --target log --overwrite
     textx generate $model --target psychopy --overwrite
 done
+
+cd counterbalancing
+./compile.sh
